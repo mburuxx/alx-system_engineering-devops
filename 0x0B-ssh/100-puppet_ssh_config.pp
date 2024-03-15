ef7078a0b7d24dcc9a1ffcb735c4dc1b5fbc8efc
mburuxx <njihiamburu.3@gmail.com>
2024-03-15 12:40:34 +0300

    Configure authentication using puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,19 +1,11 @@
 #!/usr/bin/env bash
 # client SSH configuration file using puppet
-include stdlib
-
-file_line { 'SSH Private Key':
-  path               => '/etc/ssh/ssh_config',
-  line               => '    IdentityFile ~/.ssh/school',
-  match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
-  replace            => true,
-  append_on_no_match => true
-}
-
-file_line { 'Deny Password Auth':
-  path               => '/etc/ssh/ssh_config',
-  line               => '    PasswordAuthentication no',
-  match              => '^[#]+[\s]*(?i)PasswordAuthentication[\s]+(yes|no)$',
-  replace            => true,
-  append_on_no_match => true
+file { '~/.ssh/config':
+  ensure => present,
+  mode   => '0600',
+  content => "
+    Host 52.91.127.207
+        IdentityFile ~/.ssh/school
+        PasswordAuthentication no
+  ",
 }
